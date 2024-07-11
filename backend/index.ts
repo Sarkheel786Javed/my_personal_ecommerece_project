d2db8fd57fb8e01930f2d4484f665862062bc163
sarkheel21429 <your_email@example.com>
2024-07-12 04:12:58 +0500

    m, ,mn,

--- backend/index.ts
+++ backend/index.ts
@@ -2,8 +2,8 @@ import  express ,{Request, Response} from 'express';
 import cors from 'cors'
 import 'dotenv/config';
 import mongoose from 'mongoose';
-import myUserRoute from './src/Routes/Auth'
-import connectDB from './src/dbconfig/db'
+import myUserRoute from './Routes/Auth'
+import connectDB from './dbconfig/db'
 
 connectDB()
 const app = express()
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(cors()) 
 
 ///////////////////////////
-app.use("/api/my/user", myUserRoute)
+app.use("/api/user", myUserRoute)
 /////////////////////////////
 app.get("/test", async (req:any, res: any)=>{
     res.json({ message:'Hello!'})

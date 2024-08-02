e8ce7c31fb7de8440b86968b4d908e6c24e18b05
sarkheel21429 <your_email@example.com>
2024-08-03 02:52:31 +0500

    xCCX

--- backend/controllers/ProductController/productController.ts
+++ backend/controllers/ProductController/productController.ts
@@ -1,17 +1,17 @@
-import { Request, Response, NextFunction } from 'express';
-import ProductModel from '../../model/ProductModel/ProductModel'
+import { Request, Response } from 'express';
+import Product from '../../model/ProductModel/ProductModel';
 
-const addProduct = async (req: Request, res: Response) => {
-    const productData = req.body;
-    try {
-      const newProduct = new ProductModel(productData);
-      await newProduct.save();
-      res.status(201).json({ message: 'Product added successfully' });
-    } catch (error) {
-      res.status(500).json({ message: 'Error adding product', error });
-    }
-  };
+ const addProduct = async (req: Request, res: Response) => {
+  const productData = req.body;
+  try {
+    const newProduct = new Product(productData);
+    await newProduct.save();
+    res.status(201).json({ message: 'Product added successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Error adding product', error });
+  }
+};
 module.exports = {
-    addProduct,
-    
+  addProduct,
+  
 };
--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,9 @@
-import express, { Request, Response } from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-import connectDB from './dbconfig/db';
-import myUserRoute from './Routes/Auth';
-import Product from './Routes/ProductRoutes/ProductRoutes';
-
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./dbconfig/db";
+import myUserRoute from "./Routes/Auth";
+import Product from "./Routes/ProductRoutes/ProductRoutes";
 
 connectDB();
 const app = express();
@@ -12,12 +11,13 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/user", myUserRoute);
+app.use(express.static("public"));
 app.use("/api/product", Product);
 
 app.get("/test", async (req: Request, res: Response) => {
-    res.json({ message: 'Hello!' });
+  res.json({ message: "Hello!" });
 });
 
 app.listen(7000, () => {
-    console.log("Server Started on localhost:7000");
+  console.log("Server Started on localhost:7000");
 });

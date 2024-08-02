0d56139c785d5b1653abe74cd01dd11a16f3dda2
sarkheel21429 <your_email@example.com>
2024-08-03 02:58:14 +0500

    sds

--- backend/Routes/ProductRoutes/ProductRoutes.ts
+++ backend/Routes/ProductRoutes/ProductRoutes.ts
@@ -3,6 +3,8 @@ import multer from 'multer';
 import path from 'path';
 import fs from 'fs';
 
+const { addProduct } = require ('../../controllers/ProductController/productController')
+
 const router = express.Router();
 
 const storage = multer.diskStorage({
@@ -21,16 +23,13 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 router.post('/upload-images', upload.array('images'), (req: Request, res: Response) => {
-  try {
-    const imagePaths = (req.files as Express.Multer.File[]).map((file) => ({
-      name: file.originalname,
-      path: `/images/${file.filename}`
-    }));
-    res.json({ imagePaths });
-  } catch (error) {
-    console.error('Error in /upload-images:', error);
-    res.status(500).json({ message: 'Error uploading images', error });
-  }
+  const imagePaths = (req.files as Express.Multer.File[]).map((file) => ({
+    name: file.originalname,
+    path: `/images/${file.filename}`
+  }));
+  res.json({ imagePaths });
 });
+
+router.post('/add-product', addProduct);
 
 export default router;
